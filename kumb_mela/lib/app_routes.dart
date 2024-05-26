d28ac20d11e2358c8a1c5c2663ff19de929a33f4
Sumit <sumit.kumar@insigniaconsultancy.com>
2024-05-26 10:41:09 +0530

    route added

--- kumb_mela/lib/app_routes.dart
+++ kumb_mela/lib/app_routes.dart
@@ -12,7 +12,7 @@ class AppRoutes {
   static Map<String, WidgetBuilder> getRoutes() {
     return {
       // dashBoardRoute: (ctx) => const HomeScreen(),
-      profileRoute: (ctx) => const HomePage(),
+      profileRoute: (ctx) => const ProfilePage(),
       // homeRoute: (ctx) => const HomePage(),
       // caseViewRoute: (ctx) => const CaseViewPage(),
       // loginPageRoute: (ctx) => const LoginScreen(),
